1f29e09acb7298682f6d7f9a0bf842df104607c3
najim2004 <najim10112004@gmail.com>
2026-01-25 11:18:50 +0600

    feat: Implement admin services for coin bundles, orders, and tickets, including database schema updates and file storage.

--- src/modules/admin/coin/dto/create-coin.dto.ts
+++ src/modules/admin/coin/dto/create-coin.dto.ts
@@ -5,17 +5,20 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  Min,
 } from 'class-validator';
 
 export class CreateCoinDto {
   @IsNotEmpty()
   @Transform(({ value }) => Number(value))
+  @Min(0, { message: 'Price must be at least 0' })
   @IsInt()
   price: number;
 
   @IsNotEmpty()
   @Transform(({ value }) => Number(value))
   @IsInt()
+  @Min(750, { message: 'Coin amount must be at least 750' })
   coin_amount: number;
 
   @IsOptional()
